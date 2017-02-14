240e1035afc0006e17bcee2592bd48074d3291c3
alex <alexbonavila44@gmail.com>
2017-02-14 21:51:02 +0100

    Tests

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 14/02/17
+ * Time: 21:40
+ */
+
+namespace app\Observers;
+
+
+class UserObserver
+{
+
+}
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Mail\WelcomeEmailMarkdown;
+use Event;
+use Illuminate\Auth\Events\Registered;
+use Mail;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -20,4 +24,52 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    /**
+     *
+     */
+    public function testRegisterUserSendWelcomeEmail()
+    {
+        Mail::fake();
+        $user = new \App\User();
+        $user->name = 'hola hola';
+        $user->email = 'hola@hola.com';
+        event(new Registered($user));
+        Mail::assertSent(WelcomeEmailMarkdown::class,function($mail) use ($user)  {
+            return ($mail->user->name ===  $user->name) && ($mail->user->email ===  $user->email);
+        });
+    }
+    /**
+     *
+     */
+    public function testRegisterUserSendWelcomeEmail2()
+    {
+        Event::fake();
+        $this->get('/registerUser');
+        Event::assertDispatched(Registered::class,function($event)  {
+            return $event->user->name === 'hola hola';
+        });
+    }
+
+//    public function testRegisterUserSendWelcomeEmail()
+//    {
+//        Event::fake();
+//
+//        $this->visit('/register')
+//            ->type('HOla','name')
+//            ->type('hola@hola.com', 'email')
+//            ->type('123456', 'password')
+//            ->type('123456', 'password_confirmation')
+//            ->press('register')
+//            ->seePageIs('/home')
+//            ->seeInDatabase('users', [
+//                'email' => 'hola@hola.com',
+//                'name' => 'HOla']);
+//
+//        Event::assertDispatched(Registered::class,function ($event){
+//            return $event->user->name === 'HOla';
+//        });
+//
+//    }
+    
 }
